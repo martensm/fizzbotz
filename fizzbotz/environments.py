987b672b20e6a74d4cf720caf811b852a447c5ba
Matthew Martens <matthew.s.martens@gmail.com>
2019-04-14 00:09:12 -0400

    Update Discord discriminator in description

--- fizzbotz/environments.py
+++ fizzbotz/environments.py
@@ -6,7 +6,7 @@ dotenv.load_dotenv()
 
 DEBUG = os.getenv("FIZZBOTZ_DEBUG") is not None
 DEBUG_TOKEN = os.getenv("FIZZBOTZ_DEBUG_TOKEN")
-DESCRIPTION = os.getenv("FIZZBOTZ_DESCRIPTION", "A bot written by Fission#1337")
+DESCRIPTION = os.getenv("FIZZBOTZ_DESCRIPTION", "A bot written by Fission#0001")
 PREFIX = os.getenv("FIZZBOTZ_PREFIX", "!")
 STATUS = os.getenv("FIZZBOTZ_STATUS", f"{PREFIX}help for usage")
 TOKEN = os.getenv("FIZZBOTZ_TOKEN")
